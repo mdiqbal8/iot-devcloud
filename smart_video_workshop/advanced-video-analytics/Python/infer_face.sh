75a9cc7e1144f6da053bb15d080747a8eec0aa07
Yogesh <yogesh38726@gmail.com>
2019-09-10 08:27:10 -0700

    Made minor Changed as per feedback, removed redundent files

--- smart_video_workshop/advanced-video-analytics/Python/infer_face.sh
+++
@@ -1,7 +0,0 @@
-cd $PBS_O_WORKDIR
-mkdir -p $1
-python3 interactive_face_detection.py   -m models/Transportation/object_detection/face/pruned_mobilenet_reduced_ssd_shared_weights/dldt/face-detection-adas-0001-fp16.xml \
-                            -i $2 \
-                            -o $1 \
-                            -d MYRIAD \
-                            -l ~/inference_engine_samples_build/intel64/Release/lib/libcpu_extension.so 
